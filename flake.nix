c41099dd03376ab3d72b14ec84f0e20fbd63fc6e
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2025-07-04 01:35:45 +0530

    Support arm64 machines

--- flake.nix
+++ flake.nix
@@ -1,47 +1,44 @@
 {
   description = "Patch Habitica APK to replace habitica.com";
-
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
-
-  outputs = { self, nixpkgs, ... }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        openjdk
-        apktool
-        apksigner
-        coreutils
-        bash
-      ];
-    };
-
-    packages.${system}.default = pkgs.stdenv.mkDerivation {
-      name = "patch-habitica";
-
-      src = ./.;
-
-      buildInputs = [ pkgs.makeWrapper ];
-
-      installPhase = ''
-        mkdir -p $out/bin
-        cp $src/patch-habitica.sh $out/bin/patch-habitica
-        chmod +x $out/bin/patch-habitica
-        wrapProgram $out/bin/patch-habitica \
-          --prefix PATH : ${pkgs.lib.makeBinPath [
-            pkgs.openjdk
-            pkgs.apktool
-            pkgs.apksigner
-            pkgs.coreutils
-            pkgs.bash
-          ]}
-      '';
-    };
-
-    apps.${system}.default = {
-      type = "app";
-      program = "${self.packages.${system}.default}/bin/patch-habitica";
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            openjdk
+            apktool
+            apksigner
+            coreutils
+            bash
+          ];
+        };
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "patch-habitica";
+          src = ./.;
+          buildInputs = [ pkgs.makeWrapper ];
+          installPhase = ''
+            mkdir -p $out/bin
+            cp $src/patch-habitica.sh $out/bin/patch-habitica
+            chmod +x $out/bin/patch-habitica
+            wrapProgram $out/bin/patch-habitica \
+              --prefix PATH : ${pkgs.lib.makeBinPath [
+                pkgs.openjdk
+                pkgs.apktool
+                pkgs.apksigner
+                pkgs.coreutils
+                pkgs.bash
+              ]}
+          '';
+        };
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/patch-habitica";
+        };
+      });
 }
